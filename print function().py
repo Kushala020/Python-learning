8772c6bc59b190cc5717b767de97f803b7b6d4bd
Kushala020 <kushalar020@gmail.com>
2026-09-12 22:52:20 +0530

    Add files via upload

---
+++ print function().py
@@ -0,0 +1,24 @@
+#PRINT FUNCTION
+name="john"
+age=18
+num1=10
+num2=20
+num3=30
+result=num1+num2+num3
+print(name)
+print(age)
+print(num1,num2,num3,result)
+print(num1,num2,num3, sep=" #")
+print(name,age, sep=",")
+print(f"My name is {name}.My age is {age}")
+print(f"addition of {num1},{num2} and {num3} is {result}")
+
+#MAXIMUM AND MINIMUM
+print(max(1,2,3,4,5))
+print(min(1,2,3,4,5))
+
+#ABSOLUTE VALUE
+print(abs(-20034))
+
+#POWER
+print(pow(4,5))

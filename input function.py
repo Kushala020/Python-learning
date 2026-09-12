1f773b2103221980c60511c2a64caf8882698b9e
Kushala020 <kushalar020@gmail.com>
2026-09-12 23:02:52 +0530

    Add files via upload

---
+++ input function.py
@@ -0,0 +1,16 @@
+name=input("enter your name:")
+print(name)
+number1=int(input("\nenter first number:"))
+number2=int(input("enter second number:"))
+result=number1+number2
+print(result)
+
+number1=float(input("\nenter first number:"))
+number2=float(input("enter second number:"))
+result=number1+number2
+print(result)
+
+number1=(input("\nenter first number:"))
+number2=(input("enter second number:"))
+result=number1+number2
+print(result)

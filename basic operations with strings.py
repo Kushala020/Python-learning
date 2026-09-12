eaa591dcb04d446a55ed36b8d5b260c22796a597
Kushala020 <kushalar020@gmail.com>
2026-09-12 22:32:10 +0530

    Add files via upload

---
+++ basic operations with strings.py
@@ -0,0 +1,21 @@
+# LENGTH OF A STRING
+name="kushala.R"
+print(f"lenght of a name is {len(name)}")
+
+# INDEXING OF A STRING
+name="kushi"
+print(name[0])
+print(name[4])
+
+# JOINING STRING TOGETHER OR CONCATENATION OF A STRING
+s1="Hello."
+s2="This is kushi."
+s3=s1 +" " +s2
+print(s3)
+
+#SLICING OF A STRING
+name="hello everyone"
+print(name[2:8:2])
+print(name[1:3:1])
+
+
